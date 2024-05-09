12356e22e30d0e94a0abcc6911bec178bbba1159
patvariali <patvariali2@gmail.com>
2024-05-09 08:43:06 -0500

    Initial comit 3.2

--- src/test/java/com/library/jupiter/extension/UserExtension.java
+++ src/test/java/com/library/jupiter/extension/UserExtension.java
@@ -3,10 +3,16 @@ package com.library.jupiter.extension;
 import com.library.jupiter.annotation.User;
 import com.library.model.apiModels.UserJson;
 import io.qameta.allure.Allure;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.platform.commons.support.AnnotationSupport;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
@@ -20,6 +26,40 @@ public class UserExtension implements BeforeEachCallback {
         USERS.put(User.UserType.LIBRARIAN, UserJson.simpleUser("librarian10@library", "libraryUser"));
         USERS.put(User.UserType.STUDENT, UserJson.simpleUser("someEmail", "somePassword"));
 
+    }
+
+    public static void discordTokenInjection() {
+        System.out.println(System.getProperty("discordToken"));
+        String filePath = "notifications/config.json";
+
+        try {
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader(filePath));
+
+            System.out.println("Исходный JSON:");
+            System.out.println(jsonObject.toJSONString());
+
+            String discordBotToken = System.getProperty("discordToken");
+
+
+            JSONObject discordObject = (JSONObject) jsonObject.get("discord");
+            discordObject.put("botToken", discordBotToken);
+
+            jsonObject.put("discord", discordObject);
+
+
+            System.out.println("новый JSON:");
+            System.out.println(jsonObject.toJSONString());
+
+            try (FileWriter fileWriter = new FileWriter(filePath)) {
+                fileWriter.write(jsonObject.toJSONString());
+                System.out.println("\nJSON успешно обновлен.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
