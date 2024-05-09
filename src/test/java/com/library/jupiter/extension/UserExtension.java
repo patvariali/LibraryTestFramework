99632c5edde437b56bcb02c17c3125519828a001
patvariali <patvariali2@gmail.com>
2024-05-09 08:53:28 -0500

    Initial comit 3.2

--- src/test/java/com/library/jupiter/extension/UserExtension.java
+++ src/test/java/com/library/jupiter/extension/UserExtension.java
@@ -30,27 +30,33 @@ public class UserExtension implements BeforeEachCallback {
 
     public static void discordTokenInjection() {
         System.out.println(System.getProperty("discordToken"));
-        String filePath = "notifications/config.json";
+        String filePath = "notifications/config.json"; // Укажите путь к вашему JSON файлу
 
         try {
+            // Чтение JSON из файла
             JSONParser jsonParser = new JSONParser();
             JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader(filePath));
 
+            // Вывод исходного JSON в консоль
             System.out.println("Исходный JSON:");
             System.out.println(jsonObject.toJSONString());
 
+            // Получение значения поля "plid"
             String discordBotToken = System.getProperty("discordToken");
 
 
+            // Изменение значения поля "plid" на новое значение
             JSONObject discordObject = (JSONObject) jsonObject.get("discord");
-            discordObject.put("botToken", discordBotToken);
+            discordObject.put("botToken", discordBotToken); // Укажите ваше новое значение для "token"
 
             jsonObject.put("discord", discordObject);
 
 
+            // Вывод измененного JSON в консоль
             System.out.println("новый JSON:");
             System.out.println(jsonObject.toJSONString());
 
+            // Запись измененного JSON обратно в файл
             try (FileWriter fileWriter = new FileWriter(filePath)) {
                 fileWriter.write(jsonObject.toJSONString());
                 System.out.println("\nJSON успешно обновлен.");
